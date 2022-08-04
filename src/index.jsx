39517688d406112d5c3124a4cc7b6e78b99677fd
arslonbekXX <arslonbek.lion067@gmail.com>
2022-08-04 05:59:52 +0500

    lesson-132 start-codes

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "@code2307/comfi-ui-kit-kit/lib/globals.css";
-import "@code2307/comfi-ui-kit-kit/lib/index.css";
-import { Tag } from "@code2307/comfi-ui-kit-kit/lib/components";
 import "./base.scss";
+import App from "./app";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <div>
-    <Tag variant='green'>Issue</Tag>
-    <Tag variant='orange' block>
-      Orange
-    </Tag>
-    <Tag variant='red'>Danger</Tag>
+    <App />
   </div>
 );
