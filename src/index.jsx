4f27ed6a94105414e53fdbc320040fd941d438a2
arslonbekXX <arslonbek.lion067@gmail.com>
2022-08-08 11:36:46 +0500

    lesson-133 finish-codes

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app";
 import "./base.scss";
+import Tags from "./tags";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <Tags
+    separator={<div style={{ height: 8 }} />}
+    tags={[
+      { label: "Purple", color: "purple", centered: true, pointer: true },
+      { label: "Blue", color: "blue", centered: true },
+      { label: "Orange", color: "orange", centered: true },
+      { label: "Magenta", color: "magenta", centered: true },
+      { label: "Lime", color: "lime", centered: true },
+    ]}
+  />
+);
