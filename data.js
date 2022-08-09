e978a1b1e4b95ed8a80b891dda3700dc64555f3f
Muhiddin <muhiddin@MacBook-Air-Muhiddin.local>
2022-08-09 13:21:42 +0500

    data

---
+++ data.js
@@ -0,0 +1,32 @@
+import { faker } from "@faker-js/faker";
+
+export const tabData = [
+  {
+    title: faker.address.cityName(),
+    content: faker.commerce.productDescription(),
+  },
+  {
+    title: faker.address.cityName(),
+    content: faker.commerce.productDescription(),
+  },
+  {
+    title: faker.address.cityName(),
+    content: faker.commerce.productDescription(),
+  },
+  {
+    title: faker.address.cityName(),
+    content: faker.commerce.productDescription(),
+  },
+  {
+    title: faker.address.cityName(),
+    content: faker.commerce.productDescription(),
+  },
+  {
+    title: faker.address.cityName(),
+    content: faker.commerce.productDescription(),
+  },
+  {
+    title: faker.address.cityName(),
+    content: faker.commerce.productDescription(),
+  },
+];
